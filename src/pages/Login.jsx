df110c37a47db8099cc9da03246dfb31bed96dd2
Bianca Oura <biancaoura@gmail.com>
2022-08-27 19:31:10 -0300

    Requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { func, shape } from 'prop-types';
+import submitLoginForm from '../redux/actions';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  validateEmail = (email) => (email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,3})$/i));
+
+  enableButton = () => {
+    const { email, password } = this.state;
+    const minPassLength = 6;
+
+    if (password.length >= minPassLength && this.validateEmail(email)) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.enableButton());
+  };
+
+  handleClick = (e) => {
+    e.preventDefault();
+
+    const { email } = this.state;
+    const { history: { push }, dispatch } = this.props;
+
+    dispatch(submitLoginForm(email));
+    push('/carteira');
+  };
+
+  render() {
+    const { email, password, isDisabled } = this.state;
+    return (
+      <main>
+        <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="E-mail"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+          <button type="submit" onClick={ this.handleClick } disabled={ isDisabled }>
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: shape().isRequired,
+  dispatch: func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const LOGIN_SUBMIT = 'LOGIN_SUBMIT';
+
+const submitLoginForm = (email) => ({
+  type: LOGIN_SUBMIT,
+  email,
+});
+
+export default submitLoginForm;
